d057f468573fc745376463cc5f84a4035820a07a
yudhaPratama <muhyudhapratama55@gmail.com>
2024-03-24 19:45:00 +0700

    Mini Project Koperasi

---
+++ src/main/java/com/enigma/koperasi/KoperasiApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.koperasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoperasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoperasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/koperasi/KoperasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.koperasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoperasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
